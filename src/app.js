007acb49ab16faea1ac878f09ab0e29969f11bfd
Gsousaz <doomlighthighlord@gmail.com>
2023-06-25 19:13:18 -0300

    feat: rota de criação de tweets funcionando

--- src/app.js
+++ src/app.js
@@ -1,14 +1,11 @@
 import express from "express";
 import cors from "cors";
-
 const app = express();
 const PORT = 5000;
-
 const users = [];
-
+const tweets = [];
 app.use(cors());
 app.use(express.json());
-
 app.post("/sign-up", (req, res) => {
   const newUser = {
     username: req.body.username,
@@ -16,7 +13,22 @@ app.post("/sign-up", (req, res) => {
   };
   users.push(newUser);
   console.log(users);
+  console.log(users);
   res.send(users);
+});
+
+app.post("/tweets", (req, res) => {
+  const usuario = users.find((user) => user.username === req.body.username);
+  if (usuario) {
+    const novoTweet = {
+      username: req.body.username,
+      tweet: req.body.tweet,
+    };
+    tweets.push(novoTweet);
+    res.send("OK");
+  } else {
+    res.send("UNAUTHORIZED");
+  }
 });
 
 app.listen(PORT, () => {
